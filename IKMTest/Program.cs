9e4fdc9069dcb29b0f926367c500cb936b24f415
Artem Chumak <artem_chumak@hotmail.com>
2019-02-22 13:59:42 -0800

    Add project files.

---
+++ IKMTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IKMTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
